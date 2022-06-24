907eae848e7f9065570af1e9f3b0ffe834e25760
Luis Manzo <manzosk3@gmail.com>
2022-06-23 21:43:54 -0500

    adds support poke of the same type

--- pokemon_team_ssr/pokemon_team_app/views.py
+++ pokemon_team_ssr/pokemon_team_app/views.py
@@ -11,7 +11,22 @@ def index(request):
     r = requests.get(f'https://pokeapi.co/api/v2/pokemon/{randnum}')
     response_json = r.json()
     poke_data = {
-        'name' : response_json["types"][0]["type"]["name"],
-        'image_url': response_json["sprites"]["front_default"]
+        "base_poke" : {
+                "base_poke_type": response_json["types"][0]["type"]["name"],
+                "base_poke_image_url": response_json["sprites"]["front_default"]
+            },
+        "support_poke" : []
     }
+    typeurl = response_json["types"][0]["type"]["url"]
+    r = requests.get(f'{typeurl}')
+    response_json = r.json();
+    support_poke = response_json["pokemon"]
+    for i in range(1, 5):
+        randnum = random.randint(0, len(support_poke) - 1)
+        support_poke_url = support_poke[randnum]["pokemon"]["url"]
+        r = requests.get(f'{support_poke_url}')
+        response_json = r.json()
+        
+        poke_data["support_poke"].append({ "name": response_json["name"], "image_url": response_json["sprites"]["front_default"]})
+    print(poke_data)
     return render(request, 'pages/index.html', poke_data)
